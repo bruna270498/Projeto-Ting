ac2dc55720a32082afc29e71e7ccd1a379579b07
bruna <brunaalmeida075@gmail.com>
2023-06-13 17:05:20 -0300

    requisito 3 em andamento

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,9 +1,29 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    for file in isinstance._queue:
+        if file["nome_do_arquivo"] == path_file:
+            return None
+    fileTxt = txt_importer(path_file)
+    dictionary = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(fileTxt),
+        "linhas_do_arquivo": fileTxt
+    }
+    instance.enqueue(dictionary)
+    print(dictionary)
 
 
 def remove(instance):
     """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        print('Não há elementos')
+        return None
+    data = instance.dequeue()
+    fileRemove = data["nome_do_arquivo"]
+    print(f'Arquivo {fileRemove} removido com sucesso')
 
 
 def file_metadata(instance, position):
