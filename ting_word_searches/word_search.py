784f96a4fbba0d2f6523f08055eb76926acee816
bruna <brunaalmeida075@gmail.com>
2023-06-14 18:05:52 -0300

    requisito 7 em andamento

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,20 @@
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
+    data = []
+    for arquivo in instance._queue:
+        occurrences = []
+        with open(arquivo['linhas_do_arquivo'], 'r') as file:
+            for line_number, line in enumerate(file, start=1):
+                if word.lower() in line.lower():
+                    occurrences.append({"linha": line_number})
+
+    if occurrences:
+        data.append({
+                "palavra": word,
+                "arquivo": arquivo['linhas_do_arquivo'],
+                "ocorrencias": occurrences
+            })
+    return data
 
 
 def search_by_word(word, instance):
