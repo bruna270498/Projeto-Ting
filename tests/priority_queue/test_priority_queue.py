08dc2a0793e214229126d7fc1bc9e82939728d8b
bruna <brunaalmeida075@gmail.com>
2023-06-14 17:16:49 -0300

    requisito 6 rm andamento

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,15 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
     """Aqui irá sua implementação"""
+    priority = PriorityQueue()
+    priority.enqueue({"qtd_linhas": 3})
+    priority.enqueue({"qtd_linhas": 2})
+    priority.enqueue({"qtd_linhas": 10})
+    priority.enqueue({"qtd_linhas": 4})
+    assert priority.dequeue() == {"qtd_linhas": 3}
+    assert priority.search(0) == {"qtd_linhas": 4}
+    with pytest.raises(IndexError):
+        priority.search(1)
